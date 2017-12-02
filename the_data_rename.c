1e5cc485e92b2bfe2483719f6c35b4284496cd02
Mason55 <1433293191@qq.com>
2017-12-02 16:57:35 +0800

    add the annotation

--- the_data_rename.c
+++ the_data_rename.c
@@ -5,17 +5,17 @@ struct Teacher{
     int sex;
 };
 
-
 typedef struct Teacher2{
     char name[64];
     int sex;
-}Te;
+}Teacher2;//把这个结构体重新定义为数据类型Teacher2
+
+//这里就是针对数据类型进行了重命名
 
 int main(void)
 {
-    struct Teacher t1;
-    Te t2;
+    Teacher t1;//这个是不对的,前面要有struct
+    Teacher2 t2;//这个就可以认为是一种数据类型了
     printf("Hello World!\n");
     return 0;
 }
-
